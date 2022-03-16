9394e8db46bdac8f24e9346a5ae34fca756a345e
Mohamed_Arramdani <mohamed.arramdani97@gmail.com>
2022-03-16 21:17:52 +0100

    diferent types of comments + documentation comment

--- Practice.java
+++ Practice.java
@@ -1,7 +1,12 @@
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        /**example documentation comment*/
+        System.out.println("hello world"); //single line comment
+        /* multi lines
+        comment as we 
+        can see here
+        */
     }
     
 }
