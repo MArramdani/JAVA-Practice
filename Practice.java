7d56de76e37c012c4e4fe54057c97a3b7edcb732
Mohamed_Arramdani <mohamed.arramdani97@gmail.com>
2022-03-23 22:03:21 +0100

    nested if statmenets

--- Practice.java
+++ Practice.java
@@ -35,6 +35,38 @@ public class Practice {
        int km = distance / 1000;
        int m = distance % 1000;
        System.out.println(km + "Kelometers \n" + m + "meters");
+
+       /* Probem using logical statments:
+    You are an administrator at a football club who must categorize already played games on the team's website.
+    The given program takes 3 inputs:
+    1. match status - which checks if the match is suspended ("true") or not suspended ("false")
+    2. your team's score
+    3. opposing team's score.
+    Complete the program so that if the match is suspended (the 1st input is "true"), it will output "Suspended".
+    If the match is not suspended ( the1st output is false), the following statuses should be set depending on the match result: "Won", "Lost" and "Draw".*/
+
+    Scanner read = new Scanner(System.in);
+       boolean isSuspended = read.nextBoolean();
+       int ourScore = read.nextInt();
+       int theirScore = read.nextInt();
+       read.close();
+       if(isSuspended){
+           System.out.println("Suspended");
+       }
+       else{
+           if(ourScore > theirScore){
+              System.out.println("Won"); 
+           }
+           else if(ourScore == theirScore){
+              System.out.println("Draw"); 
+           }
+           else{
+               System.out.println("Lost"); 
+           }
+       }
+
+
+
    
 }
 
