7dbccd94f5d5e582644df395c32e8e25fc716add
Mohamed_Arramdani <mohamed.arramdani97@gmail.com>
2022-03-24 20:56:43 +0100

    Practice using Logical statements including || &&

--- Practice.java
+++ Practice.java
@@ -17,10 +17,11 @@ public class Practice {
         System.out.println("My name is "+ name + " and I am " + age + " years old.");
         
         //getting user input 
-        Scanner myVar = new Scanner(System.in);
-        String inputvar = myVar.nextLine();
+        Scanner read = new Scanner(System.in);
+        String inputvar = read.nextLine();
         System.out.println("write something here: " + inputvar);
-        in.close();
+    
+
 
         //Problem using + A bartender sold 64 bottles of lemonade and 23 bottles of fanta. You need to calculate how many total bottles are sold. :
         int lemonade = 64;
@@ -29,9 +30,8 @@ public class Practice {
         System.out.println(--total); //one was free. removed 1 using decremenent
 
         //Problem: sort by Km and M from a normal distance from meters
-        Scanner read = new Scanner(System.in);
        int distance = read.nextInt();
-        read.close();
+       
        int km = distance / 1000;
        int m = distance % 1000;
        System.out.println(km + "Kelometers \n" + m + "meters");
@@ -45,11 +45,11 @@ public class Practice {
     Complete the program so that if the match is suspended (the 1st input is "true"), it will output "Suspended".
     If the match is not suspended ( the1st output is false), the following statuses should be set depending on the match result: "Won", "Lost" and "Draw".*/
 
-    Scanner read = new Scanner(System.in);
+   
        boolean isSuspended = read.nextBoolean();
        int ourScore = read.nextInt();
        int theirScore = read.nextInt();
-       read.close();
+      
        if(isSuspended){
            System.out.println("Suspended");
        }
@@ -65,9 +65,26 @@ public class Practice {
            }
        }
 
+       /*Problem  Logical statements:
+        You can win cash by playing the lottery. If the ticket number is a multiple of 10 and 9, the program outputs "You won $200".
+        If it is a multiple of 4 or 6, the program outputs "You won $50". In all other cases, there is no prize and the output is "Try again".*/
 
+      
+       int number = read.nextInt();
+       
 
+       if(number%10 == 0 && number%9 == 0){
+           System.out.println("You won $200");
+       }
+       else if(number%4 == 0 || number%6 == 0){
+           System.out.println("You won $50");
+       }
+       else{
+           System.out.println("Try again");
+       }
    
+
+       read.close();
 }
 
     
