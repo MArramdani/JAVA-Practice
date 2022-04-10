533d38de1daa2646a4533276c436fb77a895eee2
Mohamed_Arramdani <mohamed.arramdani97@gmail.com>
2022-04-10 03:59:05 +0000

    starting practice on classes

---
+++ Practice2.java
@@ -0,0 +1,17 @@
+public class Practice2 {
+
+    static void sayHello(){
+        System.out.println("Hellooo world.");
+    }
+
+
+
+    public static void main(String[] args) {
+        sayHello();
+    } //end of main
+} //end of class
+
+
+
+
+
