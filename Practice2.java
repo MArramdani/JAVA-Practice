62477051eb87fd0c7f94ca610a8bf02acd6e9bb9
Mohamed_Arramdani <mohamed.arramdani97@gmail.com>
2022-04-10 05:22:50 +0000

    using methods to count

--- Practice2.java
+++ Practice2.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 public class Practice2 {
 
     static void sayHello(){
         System.out.println("Hellooo world.");
     }
+    static void count(int x){
+        for(int i=1;i<=x;i++){
+            System.out.println("Number: "+i);
+        }
+
+    }
 
 
 
     public static void main(String[] args) {
+        Scanner read=new Scanner(System.in);
         sayHello();
+        System.out.println("enter a number and i will count to it:");
+        int coun=read.nextInt();
+        count(coun);
+
     } //end of main
 } //end of class
 
